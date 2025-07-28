3dacedd18412312a36b85ac58d5f26585cb1e498
Gleb <gleb.yandex.ru>
2025-07-28 18:42:57 +0300

    Initial Commit

---
+++ MyProjectGleb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyProjectGleb
+//
+//  Created by Gleb on 7/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
